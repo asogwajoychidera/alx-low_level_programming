fdc2ea85f906181dc84990c374f033cdaf29ad53
asogwajoychidera <joy.chidera12@gmail.com>
2023-08-05 07:59:00 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,72 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-
 
 /**
- * _isnumber - checks if string is a number
- * @s: string
- *
- * Return: On success 1.
- * If not a number, 0 is returned.
- */
-int _isnumber(char *s)
-{
-int i, check, d;
+*main - prints the minimum number of coins to make change
+* for an amount of money
+*@argc: number of arguments
+*@argv: array of arguments
+*Return: returns 1 if there is an error; else returns 0
+*/
 
-i = 0, d = 0, check = 1;
-if (*s == '-')
-i++;
-for (; *(s + i) != 0; i++)
+int main(int argc, char *argv[])
 {
-d = isdigit(*(s + i));
-if (d == 0)
-{
-check = 0;
-break;
-}
-}
-return (check);
-}
-/**
- * main - Entry point
- *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
- */
-int main(int argc, char **argv)
-{
-int j, ex, coins, cents, d;
-int c[5] = {25, 10, 5, 2, 1};
+int cents, coins = 0;
 
-ex = 1, j = 0, coins = 0;
-if (argc == 2)
+if (argc != 2)
 {
-if (_isnumber(argv[1]))
+printf("Error\n");
+return (1);
+}
+cents = atoi(argv[1]);
+while (cents > 0)
 {
-ex = 0, cents = atoi(argv[1]);
-if (cents >= 0)
+coins++;
+if ((cents - 25) >= 0)
 {
-while (cents != 0)
+cents -= 25;
+continue;
+}
+if ((cents - 10) >= 0)
 {
-d = cents / c[j];
-if (d == 0)
+cents -= 10;
+continue;
+}
+if ((cents - 5) >= 0)
 {
-j++;
+cents -= 5;
+continue;
 }
-else
+if ((cents - 2) >= 0)
 {
-coins += d;
-cents -= (d * c[j]);
+cents -= 2;
+continue;
 }
+cents--;
 }
-}
-}
-}
-if (ex == 0)
-printf("%i\n", coins);
-else
-printf("%s\n", "Error");
-return (ex);
+printf("%d\n", coins);
+return (0);
 }

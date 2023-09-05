5cc99007045ee2a7a3c83cab8be8a994574b2a0b
asogwajoychidera <joy.chidera12@gmail.com>
2023-09-05 14:29:53 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,34 +1,45 @@
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
+ * create_file - creates a file
  *
- * Return: If the function fails - -1.
- * Otherwise - 1.
+ * @filename: name of the file
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: Returns: 1 on success, -1 on failure
+ * file can not be created, file can not be written, write “fails”, etc…
+ * The created file must have those permissions: rw-------.
+ * If the file already exists, do not change the permissions.
+ * if filename is NULL return -1
+ * if text_content is NULL create an empty file
  */
-
 int create_file(const char *filename, char *text_content)
 {
-int o, w, len = 0;
+int fd, checkw, l = 0;
 
-if (filename == NULL)
+if (filename == 0)
 return (-1);
 
-if (text_content != NULL)
+fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+if (fd == -1)
+return (-1);
+
+if (text_content)
 {
-for (len = 0; text_content[len];)
-len++;
+while (text_content[l] != 0)
+l++;
+checkw = write(fd, text_content, l);
+
+if (checkw == -1)
+return (-1);
 }
 
-o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-w = write(o, text_content, len);
-
-if (o == -1 || w == -1)
-return (-1);
-
-close(o);
-
+close(fd);
 return (1);
 }
